9db8888be23479c3a60d0ece26777882da496976
Roman5917 bc2024-5 <romanyarema321123@gmail.com>
2026-03-07 10:40:10 +0200

    feat: added cross-cutting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
 import { Store } from "./store/Store.ts"
+import Test from "./pages/Test.tsx"
 
 function App() {
   const { count, increment, decrement, reset } = Store()
 
 
   return (
+
     <div>
       <span>{count}</span>
-      <button onClick={decrement}>-</button>d
+      <button onClick={decrement}>-</button>
       <button onClick={increment}>+</button>
       <button onClick={reset}>reset</button>
+      <Test />
     </div>
   )
 
