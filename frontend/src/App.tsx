aff1e29c323723758d22e227dd0a1194ea13666e
Roman5917 bc2024-5 <romanyarema321123@gmail.com>
2026-03-07 10:13:22 +0200

    feat: added zustand

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
+import { Store } from "./store/Store.ts"
 
 function App() {
+  const { count, increment, decrement, reset } = Store()
 
 
   return (
-    <>
-      <p className='text-red-500'> Start</p>
-
-    </>
+    <div>
+      <span>{count}</span>
+      <button onClick={decrement}>-</button>d
+      <button onClick={increment}>+</button>
+      <button onClick={reset}>reset</button>
+    </div>
   )
+
 }
 
 export default App
