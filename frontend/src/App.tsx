a89f0c921ae7560bfab48f6cdd94220e0408490b
Roman5917 bc2024-5 <romanyarema321123@gmail.com>
2026-03-12 10:59:47 +0200

    fix: fixed app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,17 @@ import './App.css'
 import RegistrationForm from './components/RegistrationForm';
 
 function App() {
-  const { count, increment, decrement, reset } = Store()
+
 
 
   return (
 
 
-      <div className="min-h-screen bg-gray-50 py-10">
-      {/* Викликаємо новий компонент форми */}
+    <div className="min-h-screen bg-gray-50 py-10">
+
       <RegistrationForm />
-      </div>
-    </>
+    </div>
+
   )
 
 }
