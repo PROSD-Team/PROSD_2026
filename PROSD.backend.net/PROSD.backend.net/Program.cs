9afa00c08db7e08d98c656c535d6b7f7895073f4
Анастасія Дудяк <ANASTASIIA.DUDIAK@lnu.edu.ua>
2026-03-18 02:43:02 +0200

    Налаштовано запуск додатку та підключення сервісів

--- PROSD.backend.net/PROSD.backend.net/Program.cs
+++ PROSD.backend.net/PROSD.backend.net/Program.cs
@@ -1,15 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using PROSD.backend.net.Data;
+using PROSD.backend.net.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -23,4 +27,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
