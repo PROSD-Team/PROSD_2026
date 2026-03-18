833e82cebb33f2d44d2d389900ff4ec14968ea79
Анастасія Дудяк <ANASTASIIA.DUDIAK@lnu.edu.ua>
2026-03-18 02:43:02 +0200

    Додано DTO для створення задачі

---
+++ PROSD.backend.net/PROSD.backend.net/Dtos/CreateJobRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PROSD.backend.net.Dtos
+{
+    public class CreateJobRequest
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
