acbe07a267e08e0189907d1a6aff5e145a1f75ef
Laindell <baklangg20@gmail.com>
2026-03-27 20:10:00 +0200

    feat: add Swagger for API documentation

--- PROSD.backend.net/PROSD.backend.net/Program.cs
+++ PROSD.backend.net/PROSD.backend.net/Program.cs
@@ -1,14 +1,29 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty; // Swagger буде відкриватися відразу за адресою http://localhost:PORT/
+    });
 }
 
 app.UseHttpsRedirection();
